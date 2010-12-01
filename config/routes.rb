a0926cfd06a3ec136efb4c55f1494b001d0642b8
stanley.liu <ivanmarkliu@gmail.com>
2010-12-01 17:25:04 +0800

    将CSS以及各个图片整理到themes_for_rails中

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Ironmine::Application.routes.draw do
   scope :module => "irm" do
     match 'login'=>'common#login',:as=>:login
   end
+
+  match '/demo(/index)' => 'demo#index'
+  themes_for_rails
 end
