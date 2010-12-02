d8cd1f4dc5f77132b5d2bf44f5d81fc7ecf07f4a
stanley.liu <ivanmarkliu@gmail.com>
2010-12-02 21:46:20 +0800

    product module model多语言关系设置

--- app/models/irm/product_module.rb
+++ app/models/irm/product_module.rb
@@ -1,3 +1,8 @@
 class Irm::ProductModule < ActiveRecord::Base
   set_table_name :irm_product_modules
+  has_many :product_modules_tls,:foreign_key=>"product_id"
+  attr_accessor :name, :description
+  #如果语言表里面字段不是name和description的话，需要特别指出
+  acts_as_multilingual
+  query_extend
 end
