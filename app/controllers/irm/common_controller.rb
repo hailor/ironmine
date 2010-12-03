dcb5b9bfe1c97c3c02442bc3680ad1182811abde
hailor <hailor1985@gmail.com>
2010-12-03 10:31:08 +0800

    多语言界面测试

--- app/controllers/irm/common_controller.rb
+++ app/controllers/irm/common_controller.rb
@@ -4,4 +4,19 @@ class Irm::CommonController < ApplicationController
   def login
     
   end
+
+
+  def win_edit
+
+  end
+
+  def win_update
+      @from_update = "i am from update"
+    respond_to do |format|
+
+        format.html { render({:action=>"win_edit",:format=>"html"}) }
+        format.js   { render({:action=>"win_edit",:format=>"html"}) }
+
+    end
+  end
 end
