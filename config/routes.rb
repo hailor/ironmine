54d81aff0056a2909616d7ab8a1cd6635467b590
stanley.liu <ivanmarkliu@gmail.com>
2010-12-15 13:46:20 +0800

    修复因没有引用jcrop.js导致无法裁剪

--- config/routes.rb
+++ config/routes.rb
@@ -95,9 +95,9 @@ Ironmine::Application.routes.draw do
     match '/identities/get_data(.:format)' => "identities#get_data"
 
     #global_settings
-    match '/global_settings/edit(.:format)' => "global_settings#edit", :via => :get
-    match '/global_settings/:id(.:format)' => "global_settings#update", :via => :put
     match '/global_settings(/index)(.:format)' => "global_settings#index", :via => :get
+    match '/global_settings/edit(.:format)' => "global_settings#edit", :via => :get
+    match '/global_settings/:id/update(.:format)' => "global_settings#update"
     match '/global_settings/crop(.:format)' => "global_settings#crop"
     #auth_sources
     match '/auth_sources(/index)(.:format)' => "auth_sources#index", :via => :get
