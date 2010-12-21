6420b08563fb95aa8ba4632a67d0a261cdbb88d6
eric <ginkgowoo@gmail.com>
2010-12-21 22:19:58 +0800

    增加关闭窗口的功能

--- db/migrate/irm/20101219145250_modify_position_in_flex_value.rb
+++ db/migrate/irm/20101219145250_modify_position_in_flex_value.rb
@@ -1,8 +1,8 @@
 class ModifyPositionInFlexValue < ActiveRecord::Migration
   def self.up
-    execute('drop view irm_flex_values_vl')
-    rename_column :irm_flex_values, :position, :display_sequence
-    change_column :irm_flex_values, :display_sequence, :integer, :null => false, :default => 0
+    #execute('drop view irm_flex_values_vl')
+    #rename_column :irm_flex_values, :position, :display_sequence
+    #change_column :irm_flex_values, :display_sequence, :integer, :null => false, :default => 0
     execute('CREATE OR REPLACE VIEW irm_flex_values_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
                                              FROM irm_flex_values  t,irm_flex_values_tl tl
                                              WHERE t.id = tl.flex_value_id')    
