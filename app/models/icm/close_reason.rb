ed59c89e4c7b45abbb9a915f02236d87a5d110f7
hailor <hailor1985@gmail.com>
2010-12-23 14:26:46 +0800

    添加多语言关联字段

--- app/models/icm/close_reason.rb
+++ app/models/icm/close_reason.rb
@@ -16,7 +16,7 @@ class Icm::CloseReason < ActiveRecord::Base
 
   scope :list_all,lambda{
     joins("LEFT OUTER JOIN #{Irm::LookupValue.view_name} ON  #{Irm::LookupValue.view_name}.lookup_type = 'ICM_CLOSE_REASON_TYPE' AND #{Irm::LookupValue.view_name}.id = #{table_name}.category_id AND #{Icm::CloseReasonsTl.table_name}.language = #{Irm::LookupValue.view_name}.language").
-    joins("LEFT OUTER JOIN #{Irm::Company.view_name} ON #{Irm::Company.view_name}.id = #{table_name}.company_id").
+    joins("LEFT OUTER JOIN #{Irm::Company.view_name} ON #{Irm::Company.view_name}.id = #{table_name}.company_id AND #{Icm::CloseReasonsTl.table_name}.language = #{Irm::Company.view_name}.language").
     select("#{table_name}.*,#{Irm::LookupValue.view_name}.meaning category_name,#{Icm::CloseReasonsTl.table_name}.name,#{Irm::Company.view_name}.name company_name")
   }
 end
--- app/models/icm/rule_setting.rb
+++ app/models/icm/rule_setting.rb
@@ -8,8 +8,9 @@ class Icm::RuleSetting < ActiveRecord::Base
 
 
   scope :list_all,lambda{
-    joins("LEFT OUTER JOIN #{Irm::LookupValue.view_name} ON  #{Irm::LookupValue.view_name}.lookup_type = 'ICM_PROCESS_TYPE' AND #{Irm::LookupValue.view_name}.lookup_code = #{table_name}.assignment_process_code").
     joins("LEFT OUTER JOIN #{Irm::Company.view_name} ON #{Irm::Company.view_name}.id = #{table_name}.company_id").
+    joins("LEFT OUTER JOIN #{Irm::LookupValue.view_name} ON  #{Irm::LookupValue.view_name}.lookup_type = 'ICM_PROCESS_TYPE' AND #{Irm::LookupValue.view_name}.lookup_code = #{table_name}.assignment_process_code AND #{Irm::LookupValue.view_name}.language = #{Irm::Company.view_name}.language").    
+    where("#{Irm::Company.view_name}.language = ?",I18n.locale).
     select("#{table_name}.*,#{Irm::Company.view_name}.name company_name,#{Irm::LookupValue.view_name}.meaning process_name")
   }
 
