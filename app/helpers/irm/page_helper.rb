40c75f0c89f823595053f40f35dd8b2df306a224
stanley.liu <ivanmarkliu@gmail.com>
2010-12-23 11:28:00 +0800

    更改多语言地球图标

--- app/helpers/irm/page_helper.rb
+++ app/helpers/irm/page_helper.rb
@@ -9,4 +9,8 @@ module Irm::PageHelper
     options = options.merge({:class => "formlabel"})
     content_tag(:td, obj, options)
   end
+
+  def page_td_sep(options = {})
+    content_tag(:td, raw(content_tag(:hr)), options)
+  end
 end
