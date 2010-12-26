a6ae008d7da305cf673816a3f4c6e64a348e0f64
stanley.liu <ivanmarkliu@gmail.com>
2010-12-27 00:28:13 +0800

    运营分类弹性域初始化数据

--- db/migrate/irm/20101226150155_modify_id_flex_value_set_id.rb
+++ db/migrate/irm/20101226150155_modify_id_flex_value_set_id.rb
@@ -2,7 +2,7 @@ class ModifyIdFlexValueSetId < ActiveRecord::Migration
   def self.up
     execute("drop view irm_flex_segments_vl")
     remove_column :irm_id_flex_segments, :flex_value_set_id
-    add_column :irm_id_flex_segments, :flex_value_set_name, :string, :limit => 30, :before => :status_code
+    add_column :irm_id_flex_segments, :flex_value_set_name, :string, :limit => 30, :after => :default_value
     execute('CREATE OR REPLACE VIEW irm_flex_segments_vl AS SELECT t.*,tl.id lang_id, tl.form_left_prompt, tl.form_above_prompt,tl.description,tl.language,tl.source_lang
                                              FROM irm_id_flex_segments  t,irm_id_flex_segments_tl tl
                                              WHERE t.segment_name = tl.segment_name')      
@@ -11,7 +11,7 @@ class ModifyIdFlexValueSetId < ActiveRecord::Migration
   def self.down
     execute("drop view irm_flex_segments_vl")
     remove_column :irm_id_flex_segments, :flex_value_set_name
-    add_column :irm_id_flex_segments, :flex_value_set_id, :integer, :before => :status_code
+    add_column :irm_id_flex_segments, :flex_value_set_id, :integer, :after => :default_value
     execute('CREATE OR REPLACE VIEW irm_flex_segments_vl AS SELECT t.*,tl.id lang_id, tl.form_left_prompt, tl.form_above_prompt,tl.description,tl.language,tl.source_lang
                                              FROM irm_id_flex_segments  t,irm_id_flex_segments_tl tl
                                              WHERE t.segment_name = tl.segment_name')
