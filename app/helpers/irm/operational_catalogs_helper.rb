a3c5a35f8b5dafea03bbbe53610ff1e56069fe24
stanley.liu <ivanmarkliu@gmail.com>
2010-12-28 16:06:13 +0800

    operation catalog删除多语言，只留公司和三个分类字段

--- app/helpers/irm/operational_catalogs_helper.rb
+++ app/helpers/irm/operational_catalogs_helper.rb
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 module Irm::OperationalCatalogsHelper
-  def available_oper_cata_segment_set(segment, catalog_type)
-    set_name = Irm::IdFlexSegment.query_by_structure_code(catalog_type).segment(segment.to_s).first().flex_value_set_name
+  def available_oper_cata_segment_set(segment)
+    set_name = Irm::IdFlexSegment.query_by_structure_code("OPERATIONAL_CATALOG").segment(segment.to_s).first().flex_value_set_name
     Irm::FlexValue.query_by_value_set_name(set_name).multilingual.collect{|m| [m[:flex_value_meaning], m.flex_value]}
   end
 
   def available_catalog_types
-    Irm::IdFlexStructure.query_by_flex_code("OPERATIONAL_CATALOG").multilingual.collect{|m| [m[:id_flex_structure_name], m.id_flex_structure_code]}
+    Irm::IdFlexStructure.query_by_flex_code("CATALOGS").multilingual.collect{|m| [m[:id_flex_structure_name], m.id_flex_structure_code]}
   end
 end
--- db/data/irm/20101226153847_add_operational_catalogs_id_flex_data.rb
+++ db/data/irm/20101226153847_add_operational_catalogs_id_flex_data.rb
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 class AddOperationalCatalogsIdFlexData < ActiveRecord::Migration
   def self.up
-    id_flex = Irm::IdFlex.new(:id_flex_code => "OPERATIONAL_CATALOG", :id_flex_name => "Operational Catalog Id Flexes", :status_code => "ENABLED")
+    id_flex = Irm::IdFlex.new(:id_flex_code => "CATALOGS", :id_flex_name => "Operational Catalog Id Flexes", :status_code => "ENABLED")
     id_flex.save
 
-    id_flex_structure = Irm::IdFlexStructure.new(:id_flex_code => "OPERATIONAL_CATALOG", :id_flex_num => 100, :id_flex_structure_code => "OPE_CATALOG_ICM", :status_code => "ENABLED",:not_auto_mult=>true)
-    id_flex_structure.id_flex_structures_tls.build(:id_flex_structure_name => "Operational Catalog ICM", :language => "zh", :source_lang => "zh", :status_code => "ENABLED")
-    id_flex_structure.id_flex_structures_tls.build(:id_flex_structure_name => "Operational Catalog ICM", :language => "en", :source_lang => "zh", :status_code => "ENABLED")
+    id_flex_structure = Irm::IdFlexStructure.new(:id_flex_code => "CATALOGS", :id_flex_num => 100, :id_flex_structure_code => "OPERATIONAL_CATALOG", :status_code => "ENABLED", :concatenated_segment_delimiter => '-',:not_auto_mult=>true)
+    id_flex_structure.id_flex_structures_tls.build(:id_flex_structure_name => "Operational Catalog", :language => "zh", :source_lang => "zh", :status_code => "ENABLED")
+    id_flex_structure.id_flex_structures_tls.build(:id_flex_structure_name => "Operational Catalog", :language => "en", :source_lang => "zh", :status_code => "ENABLED")
     id_flex_structure.save
 
-    id_flex_segment1 = Irm::IdFlexSegment.new(:id_flex_code => "OPERATIONAL_CATALOG", :id_flex_num => 100, :segment_num => 10,
+    id_flex_segment1 = Irm::IdFlexSegment.new(:id_flex_code => "CATALOGS", :id_flex_num => 100, :segment_num => 10,
                                               :segment_name => "segment1", :status_code => "ENABLED", :flex_value_set_name => "OPERATIONAL_CATALOG_SYSTEM",:not_auto_mult=>true)
     id_flex_segment1.id_flex_segments_tls.build(:segment_name => "segment1",:form_left_prompt => "系统", :status_code => "ENABLED", :language => "zh", :source_lang => "zh")
     id_flex_segment1.id_flex_segments_tls.build(:segment_name => "segment1",:form_left_prompt => "系统", :status_code => "ENABLED", :language => "en", :source_lang => "zh")
     id_flex_segment1.save
 
-    id_flex_segment2 = Irm::IdFlexSegment.new(:id_flex_code => "OPERATIONAL_CATALOG", :id_flex_num => 100,:segment_num => 20,
+    id_flex_segment2 = Irm::IdFlexSegment.new(:id_flex_code => "CATALOGS", :id_flex_num => 100,:segment_num => 20,
                                               :segment_name => "segment2", :status_code => "ENABLED", :flex_value_set_name => "OPERATIONAL_CATALOG_MODULE",:not_auto_mult=>true)
     id_flex_segment2.id_flex_segments_tls.build(:segment_name => "segment2",:form_left_prompt => "模块", :status_code => "ENABLED", :language => "zh", :source_lang => "zh")
     id_flex_segment2.id_flex_segments_tls.build(:segment_name => "segment2",:form_left_prompt => "模块", :status_code => "ENABLED", :language => "en", :source_lang => "zh")
     id_flex_segment2.save
 
-    id_flex_segment3 = Irm::IdFlexSegment.new(:id_flex_code => "OPERATIONAL_CATALOG", :id_flex_num => 100,:segment_num => 30,
+    id_flex_segment3 = Irm::IdFlexSegment.new(:id_flex_code => "CATALOGS", :id_flex_num => 100,:segment_num => 30,
                                               :segment_name => "segment3", :status_code => "ENABLED", :flex_value_set_name => "OPERATIONAL_CATALOG_FEATURE",:not_auto_mult=>true)
     id_flex_segment3.id_flex_segments_tls.build(:segment_name => "segment3",:form_left_prompt => "功能", :status_code => "ENABLED", :language => "zh", :source_lang => "zh")
     id_flex_segment3.id_flex_segments_tls.build(:segment_name => "segment3",:form_left_prompt => "功能", :status_code => "ENABLED", :language => "en", :source_lang => "zh")
