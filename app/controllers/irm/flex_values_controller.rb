92213466044df2a838880b4bf96ce28581c6f02b
stanley.liu <ivanmarkliu@gmail.com>
2011-01-07 11:41:33 +0800

    flex value 页面

--- app/controllers/irm/flex_values_controller.rb
+++ app/controllers/irm/flex_values_controller.rb
@@ -14,18 +14,18 @@ class Irm::FlexValuesController < ApplicationController
     @flex_value = Irm::FlexValue.find(params[:id])
     respond_to do |format|
       if @flex_value.update_attributes(params[:irm_flex_value])
-        flash[:successful_message] = (t :successfully_updated)
-        format.html { render "successful_info" }
+        format.html { redirect_to({:action=>"index", :value_set_id => @flex_value.flex_value_set_id}, :notice => t(:successfully_updated)) }
+        format.xml  { head :ok }
       else
-        @error = @flex_value
-        format.html { render "error_message" }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @flex_value.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def new
+    @value_set = Irm::FlexValueSet.find(params[:value_set_id]) if params[:value_set_id]
     @flex_value = Irm::FlexValue.new
-    @value_set = Irm::FlexValueSet.find(params[:value_set_id])
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @flex_value }
@@ -34,55 +34,16 @@ class Irm::FlexValuesController < ApplicationController
 
   def create
     @flex_value = Irm::FlexValue.new(params[:irm_flex_value])
-    @value_set = Irm::FlexValueSet.find(params[:value_set_id])
-    @flex_value.value_set_code = @value_set.value_set_code
+#    @flex_value.value_set_code = @value_set.value_set_code
     respond_to do |format|
       if @flex_value.save
-        flash[:successful_message] = (t :successfully_created)
-        format.html { render "successful_info" }
+        format.html { redirect_to({:action=>"index", :value_set_id => @flex_value.flex_value_set_id}, :notice =>t(:successfully_created)) }
+        format.xml  { render :xml => @flex_value, :status => :created, :location => @flex_value }
       else
-         @error = @flex_value
-         format.html { render "error_message" }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @flex_value.errors, :status => :unprocessable_entity }
       end
     end
-  end
-
-  def create_value
-    value_set = Irm::FlexValueSet.find(params[:value_set_id])
-    display_sequence = params["c1"]
-    flex_value = params["c2"]
-    name   = params["c3"]
-    description = params["c4"]
-    start_date_active = params["c5"]
-    end_date_active = params["c6"]
-
-    @mode = params["!nativeeditor_status"]
-
-    @id = params["gr_id"]
-    case @mode
-      when "inserted"
-        @flex_value = Irm::FlexValue.new({:flex_value_set_id => value_set.id, :display_sequence => display_sequence, :flex_value => flex_value,
-                                          :flex_value_meaning => name, :description => description, :start_date_active => start_date_active, :end_date_active => end_date_active})
-        @flex_value.save
-        @id = @flex_value.id
-        @tid = @flex_value.id
-      when "updated"
-        exist_flag = Irm::FlexValue.check_flex_value_exist(@id)
-        #no exists
-        if !exist_flag
-          @flex_value = Irm::FlexValue.new({:flex_value_set_id => value_set.id, :display_sequence => display_sequence, :flex_value => flex_value,
-                                          :flex_value_meaning => name, :description => description, :start_date_active => start_date_active, :end_date_active => end_date_active})
-          @flex_value.save
-          @id = @flex_value.id
-          @tid = @flex_value.id
-        else
-          @flex_value=Irm::FlexValue.find(@id)
-          @flex_value.update_attributes({:flex_value_set_id => value_set.id, :display_sequence => display_sequence, :flex_value => flex_value,
-                                          :flex_value_meaning => name, :description => description, :start_date_active => start_date_active, :end_date_active => end_date_active})
-          @id = @flex_value.id
-          @tid = @flex_value.id
-        end
-    end    
   end
 
   def get_data
@@ -93,34 +54,6 @@ class Irm::FlexValuesController < ApplicationController
     respond_to do |format|
       format.json  {render :json => to_jsonp(flex_values.to_grid_json([:display_sequence,:flex_value,:flex_value_meaning,:description,:start_date_active,:end_date_active,], count)) }
     end    
-  end
-
-  def link_edit
-    respond_to do |format|
-      if params[:value_set_id]
-        format.html{redirect_to(:controller => "irm/flex_values", :action => "index", :value_set_id => params[:value_set_id])}
-      else
-        format.html{render("error_message")}
-      end
-    end
-  end
-
-  def multilingual_edit
-    @flex_value = Irm::FlexValue.find(params[:id])
-  end
-
-  def multilingual_update
-    @flex_value = Irm::FlexValue.find(params[:id])
-    @flex_value.not_auto_mult=true
-    respond_to do |format|
-      if @flex_value.update_attributes(params[:irm_flex_value])
-        format.html { redirect_to({:action=>"multilingual_edit",:format=>"js"}, :notice => t(:successfully_updated)) }
-        format.js { redirect_to({:action=>"multilingual_edit",:format=>"js"}, :notice => t(:successfully_updated)) }
-      else
-        format.html { render({:action=>"multilingual_edit"}) }
-        format.js   { render({:action=>"multilingual_edit"}) }
-      end
-    end
   end
 
   def destroy
@@ -138,6 +71,10 @@ class Irm::FlexValuesController < ApplicationController
   end
 
   def show
-    
+    @flex_value = Irm::FlexValue.multilingual.find(params[:id])
+  end
+
+  def select_set
+    redirect_to :action => "index", :value_set_id => params[:flex_value_set][:value_set_id]
   end
 end
