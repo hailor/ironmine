4c5b672bd8142d0fd08f13734e6cb0e69830f3d4
stanley.liu <ivanmarkliu@gmail.com>
2011-01-10 15:05:00 +0800

    menu entry页面

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 module ApplicationHelper
+  def page_title(title = "", description = "")
+    title_tags = ""
+    if @current_menu_entry && @current_menu_entry.permission_code
+      permission = Irm::Permission.list_all.where(:id => @current_permission.id).first
+      if @current_menu_entry.icon
+        title_tags << content_tag(:img, "", :src => '/images/s.gif', :class => @current_menu_entry.icon + " pageTitleIcon")
+      end
+      if title.blank?
+        title_tags << title
+      else
+        title_tags << content_tag(:h1, "", :class => "pageType")
+      end
+      if description.blank?
+        title_tags << description
+      else
+        title_tags << content_tag(:h2, permission[:name], :class => "pageDescription")
+      end
+    end
+    content = raw(content_tag(:div, raw(title_tags), :class => "content"))
+    pt_body = raw(content_tag(:div, content, :class => "ptBody"))
+    b_page_title = raw(content_tag(:div, pt_body, :class => "bPageTitle"))
+    raw(b_page_title)
+  end
+  
   #显示form提交的出错信息
   def error_message_for(*args)
     lis=""
