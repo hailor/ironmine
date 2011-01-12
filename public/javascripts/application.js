772812936e9e82e2488bb7fd9348e0aa4dc28ea1
hailor <hailor1985@gmail.com>
2011-01-12 21:33:05 +0800

    添加irm模块js事件发布机制，解决表格慢而无法初始化filter的问题

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,19 @@ YUI.add('irm', function(Y) {
 
     Y.namespace('irm');
 
+    Y.irm.publisher = new Y.EventTarget();
+    Y.irm.publisher.publish('irm:change', {
+      broadcast: 1,   // instance level notification
+      emitFacade: true // emit a facade so we get the event target
+    });
+
+    Y.irm.setAttribute = function(name,value,type){
+      this[name] = value;
+      Y.irm.publisher.fire("irm:change",name,value,type);
+    };
+
     Y.irm.handleClick = function(){
         Y.one("#content").addClass("ccc");
-        alert("hello");
       };
     // {tbody, tr, td, classnames, headers, rowindex, record, column, data, value}
     Y.irm.template = function(o){
@@ -114,10 +124,35 @@ YUI.add('irm', function(Y) {
               selectedNode.addClass("setupHighlightLeaf");
             }
           }
-    }
+    };
+    Y.irm.ViewFilter = function (domid){
+       Y.on("irm:change",function(e){
+           Y.one("#"+domid).all("select.viewFilter").each(function(n){
+             if(e.details[2]=="Datatable"&&n.getAttribute("ref")==e.details[0])
+             {
+               var datasource = e.details[1].datasource;
+               Y.one("#"+n.get("id")+"Overview").setStyle("display","block");
+               datasource.filter({_view_filter_id:n.get("value")});
+               n.on("change",function(e){
+                   datasource.filter({_view_filter_id:n.get("value")});
+               });
+               Y.one("#"+n.get("id")+"EditLink").on("click",function(e){
+                   if(!this.getAttribute("thref")){
+                      this.setAttribute("thref",this.getAttribute("href"));
+                   }
+                   var href = unescape(this.getAttribute("thref"));
+                   href = Y.Lang.substitute(href,{id:n.get("value")});
+                   this.setAttribute("href",href);
+                   if(!n.get("value"))
+                    e.preventDefault()
+               });
+             }
+           });
+       });
+    };
 
 }, '0.1.1' /* module version */, {
-    requires: ['base',"overlay","node-event-simulate","event-mouseenter"]
+    requires: ['base',"overlay","node-event-simulate","event-custom","event-mouseenter"]
 });
 
 GY.use('node',function(Y){
