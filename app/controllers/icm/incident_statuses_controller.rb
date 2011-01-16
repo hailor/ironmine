adddb46a93a09606433fe619525629156a8d014d
hailor <hailor1985@gmail.com>
2011-01-16 11:38:51 +0800

    修改事故单状态界面

--- app/controllers/icm/incident_statuses_controller.rb
+++ app/controllers/icm/incident_statuses_controller.rb
@@ -9,7 +9,7 @@ class Icm::IncidentStatusesController < ApplicationController
   end
 
   def show
-    @incident_status = Icm::IncidentStatus.multilingual.list_all(I18n::locale).find(params[:id])
+    @incident_status = Icm::IncidentStatus.multilingual.with_phase.status_meaning.find(params[:id])
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @incident_status }
@@ -26,17 +26,16 @@ class Icm::IncidentStatusesController < ApplicationController
   end
 
   def edit
-    @incident_status = Icm::IncidentStatus.multilingual.list_all(I18n::locale).find(params[:id])
+    @incident_status = Icm::IncidentStatus.multilingual.find(params[:id])
   end
 
   def create
     @incident_status = Icm::IncidentStatus.new(params[:icm_incident_status])
     respond_to do |format|
       if @incident_status.save
-        flash[:successful_message] = (t :successfully_created)
-        format.html { render "index" }
+         format.html { redirect_to({:action=>"index"}, :notice =>t(:successfully_created)) }
       else
-         format.html { render "new" }
+         format.html { render :action=>"new" }
       end
     end
   end
@@ -46,10 +45,9 @@ class Icm::IncidentStatusesController < ApplicationController
 
     respond_to do |format|
       if @incident_status.update_attributes(params[:icm_incident_status])
-        flash[:successful_message] = (t :successfully_updated)
-        format.html { render "index" }
+        format.html { redirect_to({:action=>"index"}, :notice =>t(:successfully_updated)) }
       else
-        format.html { render "edit" }
+        format.html { render :action=>"edit" }
       end
     end
   end
@@ -71,10 +69,13 @@ class Icm::IncidentStatusesController < ApplicationController
   end
 
   def get_data
-    @incident_statuses = Icm::IncidentStatus.multilingual.list_all(I18n::locale)
-    @incident_statuses,count = paginate(@incident_statuses)
+    incident_statuses_scope = Icm::IncidentStatus.multilingual.with_phase.status_meaning
+    incident_statuses_scope = incident_statuses_scope.match_value("#{Icm::IncidentPhase.view_name}.name",params[:phase_name])
+    incident_statuses_scope = incident_statuses_scope.match_value("#{Icm::IncidentStatus.table_name}.incident_status_code",params[:incident_status_code])
+    incident_statuses_scope = incident_statuses_scope.match_value("#{Icm::IncidentStatusesTl.table_name}.name",params[:name])
+    incident_statuses,count = paginate(incident_statuses_scope)
     respond_to do |format|
-      format.json  {render :json => to_jsonp(@incident_statuses.to_grid_json(['0',:company_name,:incident_status_code,:name,:description,:status_meaning], count)) }
+      format.json  {render :json => to_jsonp(incident_statuses.to_grid_json([:phase_name,:incident_status_code,:name,:display_sequence,:default_flag,:status_meaning], count)) }
     end
   end    
 end
--- app/helpers/icm/incident_statuses_helper.rb
+++ app/helpers/icm/incident_statuses_helper.rb
@@ -1,2 +1,5 @@
 module Icm::IncidentStatusesHelper
+  def available_incident_phase()
+    Icm::IncidentPhase.multilingual.enabled.collect{|v| [v[:name],v[:phase_code]]}
+  end
 end
