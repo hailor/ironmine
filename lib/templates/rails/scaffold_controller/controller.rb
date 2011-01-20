e6bdd7afb31d60c698b6d7053d80857a53abe78d
hailor <hailor1985@gmail.com>
2011-01-20 22:57:25 +0800

    修改controller的生成脚本

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -44,7 +44,7 @@ class <%= controller_class_name %>Controller < ApplicationController
 
     respond_to do |format|
       if @<%= orm_instance.save %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully created.') }
+        format.html { redirect_to(@<%= singular_table_name %>, :notice => t(:successfully_created)) }
         format.xml  { render :xml => @<%= singular_table_name %>, :status => :created, :location => @<%= singular_table_name %> }
       else
         format.html { render :action => "new" }
@@ -60,7 +60,7 @@ class <%= controller_class_name %>Controller < ApplicationController
 
     respond_to do |format|
       if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully updated.') }
+        format.html { redirect_to(@<%= singular_table_name %>, :notice => t(:successfully_updated) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
