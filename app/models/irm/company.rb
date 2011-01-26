115f76e76410d7ff2050aeb5b5d78ab7e6bc9ff1
eric <ginkgowoo@gmail.com>
2011-01-26 17:51:19 +0800

    修改操作列的宽度

--- app/models/irm/company.rb
+++ app/models/irm/company.rb
@@ -8,7 +8,7 @@ class Irm::Company < ActiveRecord::Base
   has_many :companies_tls,:dependent => :destroy
   acts_as_multilingual
 
-  validates_presence_of :short_name
+  validates_presence_of :short_name,:company_type
   validates_uniqueness_of :short_name,:if => Proc.new { |i| !i.short_name.blank? }
   validates_format_of :short_name, :with => /^[A-Z0-9_]*$/ ,:if=>Proc.new{|i| !i.short_name.blank?}
 
