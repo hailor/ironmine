3c007789f1e4406d86b8c5cfc81b56046c76fe08
hailor <hailor1985@gmail.com>
2011-01-28 22:00:18 +0800

    在session保存top menu,用来确定显示的菜单分支

--- app/controllers/irm/setting_controller.rb
+++ app/controllers/irm/setting_controller.rb
@@ -4,7 +4,7 @@ class Irm::SettingController < ApplicationController
   end
 
   def common
-
     @setting_menus = Irm::MenuManager.parent_menus_by_menu(params[:mc],session[:top_menu])+[params[:mc]]
+    session[:top_menu] = @setting_menus[1] if @setting_menus[1]
   end
 end
