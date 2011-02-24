0fe0a530382357da73921534a162febaac4cdd3e
stanley.liu <ivanmarkliu@gmail.com>
2011-02-24 15:32:44 +0800

    fix file management file size display bug

--- app/models/irm/attachment.rb
+++ app/models/irm/attachment.rb
@@ -6,7 +6,7 @@ class Irm::Attachment < ActiveRecord::Base
   has_many :attachment_versions, :dependent => :destroy
   query_extend
   scope :list_all, lambda{
-    select("#{table_name}.*, av.id version_id, av.category_id category_id, av.data_file_name data_file_name, av.data_content_type data_content_type, av.data_file_size data_file_size, av.data_updated_at data_updated_at, fvt.meaning category_name").
+    select("#{table_name}.*, av.id version_id, av.category_id category_id, av.data_file_name data_file_name, av.data_content_type data_content_type, av.data_file_size/1024 data_file_size, av.data_updated_at data_updated_at, fvt.meaning category_name").
     joins(", #{Irm::AttachmentVersion.table_name} av").
     joins(", #{Irm::LookupValue.table_name} fv").
     joins(", #{Irm::LookupValuesTl.table_name} fvt").
