2cd4e0b71f3112abe9daebecce1d94c7142b6992
stanley.liu <ivanmarkliu@gmail.com>
2011-02-24 15:23:39 +0800

    fix file management migration bug

--- db/data/skm/20110224063056_add_file_management_menu_data2.rb
+++ db/data/skm/20110224063056_add_file_management_menu_data2.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class AddFileManagementMenuData2 < ActiveRecord::Migration
   def self.up
-    skm_menu_entiry_85= Irm::MenuEntry.new(:menu_code=>'IRM_SOC_FILE_MANAGEMENT_MENU',:sub_menu_code=>nil,:permission_code=>'SKM_FILE_MANAGEMENTs_INDEX',:display_sequence=>10,:display_flag=>'Y',:not_auto_mult=>true)
+    skm_menu_entiry_85= Irm::MenuEntry.new(:menu_code=>'IRM_SOC_FILE_MANAGEMENT_MENU',:sub_menu_code=>nil,:permission_code=>'SKM_FILE_MANAGEMENTS_INDEX',:display_sequence=>10,:display_flag=>'Y',:not_auto_mult=>true)
     skm_menu_entiry_85.menu_entries_tls.build(:language=>'zh',:source_lang=>'en',:name=>'文件管理',:description=>'文件管理')
     skm_menu_entiry_85.menu_entries_tls.build(:language=>'en',:source_lang=>'en',:name=>'File Management',:description=>'File Management')
     skm_menu_entiry_85.save
