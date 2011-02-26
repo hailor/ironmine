926adcd7e1a52addcf9666b704b8a783da761e5d
eric <ginkgowoo@gmail.com>
2011-02-26 17:02:03 +0800

    development report

--- db/data/irm/20110227013325_init_irm_report_menu.rb
+++ db/data/irm/20110227013325_init_irm_report_menu.rb
@@ -16,6 +16,13 @@ class InitIrmReportMenu < ActiveRecord::Migration
     report_menu_list_87.menu_entries_tls.build(:language=>'zh',:source_lang=>'en',:name=>'报表',:description=>'报表')
     report_menu_list_87.menu_entries_tls.build(:language=>'en',:source_lang=>'en',:name=>'Report',:description=>'Report')
     report_menu_list_87.save
+
+    #将所有的事故单模块的报表都挂在报表的菜单下
+    report_menu_list_88= Irm::MenuEntry.new(:menu_code=>'IRM_REPORT_LISTS_MENU',:sub_menu_code=>nil,
+                                            :permission_code=>'ICM_INCIDENT_REPORTS_INDEX',:display_sequence=>10,:display_flag=>'Y',:not_auto_mult=>true)
+    report_menu_list_88.menu_entries_tls.build(:language=>'zh',:source_lang=>'en',:name=>'事故单报表',:description=>'事故单报表')
+    report_menu_list_88.menu_entries_tls.build(:language=>'en',:source_lang=>'en',:name=>'IncidentReport',:description=>'IncidentReport')
+    report_menu_list_88.save
   end
 
   def self.down
