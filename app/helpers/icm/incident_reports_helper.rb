7e623719b72a84398be38cccf02d42816a6b21d1
eric <ginkgowoo@gmail.com>
2011-03-01 10:48:20 +0800

    development report

--- app/helpers/icm/incident_reports_helper.rb
+++ app/helpers/icm/incident_reports_helper.rb
@@ -1,8 +1,9 @@
 module Icm::IncidentReportsHelper
    def show_module_report(category_code,options={})
       report_module = ""
+      report_limit = options[:report_limit]
       all_reports = Irm::Report.multilingual.query_by_group_and_category("ADMIN_REPORT_GROUP",category_code).
-                                limit(5)
+                                limit(report_limit)
       all_reports.each do |report|
          report_permission = Irm::Permission.query_by_permission_code(report[:permission_code]).first
          report_controller = report_permission[:page_controller]||options[:controller]
