6ac378da62d31f2c9bde0e5d3be817fb9de30d05
hailor <hailor1985@gmail.com>
2011-03-01 14:17:23 +0800

    配置邮件发送信息

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,18 @@ Ironmine::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+		:address => "smtp.gmail.com",
+		:port => 587,
+		:domain => 'mail.google.com',
+		:user_name => 'root.ironmine@gmail.com',
+		:password => 'handhand',
+		:authentication => 'plain',
+		:enable_starttls_auto => true
+  }
 end
 
