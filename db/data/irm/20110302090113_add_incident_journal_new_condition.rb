81dd3f3f28eb5405be02abda8a12d5b86a148402
hailor <hailor1985@gmail.com>
2011-03-02 17:41:40 +0800

    添加回复事故单的condition

---
+++ db/data/irm/20110302090113_add_incident_journal_new_condition.rb
@@ -0,0 +1,11 @@
+class AddIncidentJournalNewCondition < ActiveRecord::Migration
+  def self.up
+    incident_journal_new = Irm::Condition.new(:context_code=>"INCIDENT_JOURNAL",:condition_code=>"INCIDENT_JOURNAL_NEW",:not_auto_mult=>true)
+    incident_journal_new.conditions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'回复事故单',:description=>'回复事故单')
+    incident_journal_new.conditions_tls.build(:language=>'en',:source_lang=>'en',:name=>'Reply incident request',:description=>'Reply incident request')
+    incident_journal_new.save
+  end
+
+  def self.down
+  end
+end
