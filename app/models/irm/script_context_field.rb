6346dc8dece8bcf57ef7fec6e9aec96d508df6fc
hailor <hailor1985@gmail.com>
2011-03-02 17:41:01 +0800

    为邮件模板添加参数查询功能

--- app/models/irm/script_context_field.rb
+++ app/models/irm/script_context_field.rb
@@ -8,12 +8,12 @@ class Irm::ScriptContextField < ActiveRecord::Base
 
 
   validates_presence_of :context_code
-  validates_uniqueness_of :context_code, :if => Proc.new { |i| !i.context_code.blank? }
-  validates_format_of :context_code, :with => /^[A-Z0-9_]*$/ ,:if=>Proc.new{|i| !i.context_code.blank?}
+  validates_uniqueness_of :field_key,:scope=>[:context_code], :if => Proc.new { |i| !i.context_code.blank? }
 
   #加入activerecord的通用方法和scope
   query_extend
 
 
   scope :query_by_context_code,lambda{|context_code| where(:context_code=>context_code)}
+
 end
