a22f67875d7c33294b99bc4e3b1330b979d9edcf
stanley.liu <ivanmarkliu@gmail.com>
2011-03-08 15:45:37 +0800

    Add icon button in permission index

--- app/helpers/irm/page_helper.rb
+++ app/helpers/irm/page_helper.rb
@@ -18,11 +18,35 @@ module Irm::PageHelper
     content_tag(:span, "*", {:style => "color:red;"})
   end
 
-  def btn_add_image
-    content_tag(:img, "", {:class => "btn_edit;"})
+  def btn_add_icon
+    content_tag(:img, "", {:class => "btn_add", :src => "/images/s.gif"})
   end
 
-  def btn_edit_image
-    content_tag(:img, "", {:class => "btn_edit", :src => "/images/s.gif"})
+  def btn_edit_icon
+    content_tag(:img, "", {:class => "btn_edit", :src => "/images/s.gif", :title => t(:edit)})
+  end
+
+  def btn_show_icon
+    content_tag(:img, "", {:class => "btn_show", :src => "/images/s.gif", :title => t(:show)})
+  end
+
+  def btn_delete_icon
+    content_tag(:img, "", {:class => "btn_delete", :src => "/images/s.gif", :title => t(:delete)})
+  end
+
+  def icon_link_add(args = {}, block = {})
+    link_to(btn_add_icon, args, {})
+  end
+
+  def icon_link_show(args = {}, block = {})
+    link_to(btn_show_icon, args, {})
+  end
+
+  def icon_link_edit(args = {}, block = {})
+    link_to(btn_edit_icon, args, {})
+  end
+
+  def icon_link_delete(args = {}, block = {})
+    link_to(btn_delete_icon, args, {})
   end
 end
