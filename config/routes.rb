b617c6ee51d50040da7d9f6dc9f32915debf79d8
stanley.liu <ivanmarkliu@gmail.com>
2011-03-10 10:25:48 +0800

    icm其它页面改造

--- config/routes.rb
+++ config/routes.rb
@@ -495,6 +495,8 @@ Ironmine::Application.routes.draw do
     match '/incident_phases/create(.:format)' => "incident_phases#create", :via => :post
     match '/incident_phases/get_data(.:format)' => "incident_phases#get_data"
     match '/incident_phases/:id/show(.:format)' => "incident_phases#show", :via => :get
+    match '/incident_phases/:id/multilingual_edit(.:format)' => "incident_phases#multilingual_edit", :via => :get
+    match '/incident_phases/:id/multilingual_update(.:format)' => "incident_phases#multilingual_update", :via => :put    
     #incident_statuses
     match '/incident_statuses(/index)(.:format)' => "incident_statuses#index", :via => :get
     match '/incident_statuses/:id/edit(.:format)' => "incident_statuses#edit", :via => :get
