4104df3905d0fbf94f619c0859fcefe44a37dade
stanley.liu <ivanmarkliu@gmail.com>
2011-03-11 16:02:07 +0800

    首页工作日历显示

--- app/controllers/irm/home_controller.rb
+++ app/controllers/irm/home_controller.rb
@@ -1,5 +1,10 @@
 class Irm::HomeController < ApplicationController
   def index
-    
+    @month = (params[:month] || (Time.zone || Time).now.month).to_i
+    @year = (params[:year] || (Time.zone || Time).now.year).to_i
+
+    @shown_month = Date.civil(@year, @month)
+
+    @event_strips = Irm::CalendarTask.event_strips_for_month(@shown_month)    
   end
 end
