e5517ce02a7159efd76908f34ecfeff819f92418
hailor <hailor@liu-hailormatoMacBook-Pro.local>
2011-03-20 23:16:15 +0800

    添加修改个人密码页面

--- app/controllers/irm/my_info_controller.rb
+++ app/controllers/irm/my_info_controller.rb
@@ -42,33 +42,4 @@ class Irm::MyInfoController < ApplicationController
     end
   end
 
-  # 个人密码修改页面
-  def edit_password
-    @identity = Irm::Identity.current
-  end
-
-  # 更新个人密码
-  def update_password
-    @identity = Irm::Identity.current
-    params[:irm_identity][:password]="*" if params[:irm_identity][:password].blank?
-    respond_to do |format|
-      if(params[:irm_identity][:old_password]&&check_password(params[:irm_identity][:old_password]))
-        if @identity.update_attributes(params[:irm_identity])
-          format.html {redirect_to({:action=>"index"},:notice=>t(:successfully_updated))}
-        else
-          @identity.password = "" if @identity.password.eql?("*")
-          format.html {render("edit_password")}
-        end
-      else
-        @identity.errors.add(:old_password,t('activerecord.errors.messages.invalid'))
-        format.html { render("edit_password")}
-      end
-    end
-  end
-
-  private
-
-  def check_password(password)
-    Irm::Identity.current.hashed_password.eql?(Irm::Identity.hash_password(password))
-  end
 end
