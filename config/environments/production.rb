e2faf6e8a72fb59788b605e10c9bc40b50e09968
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-03-24 01:01:17 +0800

    my info edit 头像上传

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,28 +1,5 @@
 Ironmine::Application.configure do
 
-  config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
-
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
-
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
-
-  #config.time_zone = 'Beijing'
-  config.active_record.default_timezone = :local
-
-
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
@@ -43,7 +20,7 @@ Ironmine::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
