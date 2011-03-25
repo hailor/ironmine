2baf4824db68f872e66405ecffe5d96e41a33982
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-03-25 09:59:01 +0800

    production env setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,33 @@ Ironmine::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+
+
+  ##
+  config.cache_classes = false
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  #config.time_zone = 'Beijing'
+  config.active_record.default_timezone = :local
+  ##
+
+
     # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
