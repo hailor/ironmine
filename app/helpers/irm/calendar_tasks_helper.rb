0515d208bc1c5d5c1a7ecfbd4586c7698a6124fc
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-04-08 23:50:21 +0800

    重新引入event calendar插件，并建立 irm_calendar_tasks 表

--- app/helpers/irm/calendar_tasks_helper.rb
+++ app/helpers/irm/calendar_tasks_helper.rb
@@ -1,4 +1,4 @@
-module CalendarTaskHelper
+module Irm::CalendarTasksHelper
   def month_link(month_date)
     link_to(I18n.localize(month_date, :format => "%B"), {:month => month_date.month, :year => month_date.year})
   end
@@ -11,14 +11,19 @@ module CalendarTaskHelper
       :event_strips => @event_strips,
       :month_name_text => I18n.localize(@shown_month, :format => "%B %Y"),
       :previous_month_text => "<< " + month_link(@shown_month.prev_month),
-      :next_month_text => month_link(@shown_month.next_month) + " >>"    }
+      :next_month_text => month_link(@shown_month.next_month) + " >>",
+      :use_all_day => true
+    }
   end
 
   def event_calendar
     # args is an argument hash containing :event, :day, and :options
     calendar event_calendar_opts do |args|
-      event = args[:event]
-      %(<a href="/events/#{event.id}" title="#{h(event.name)}">#{h(event.name)}</a>)
+      event, day = args[:event], args[:day]
+      html = %(<a href="/events/#{event.id}" title="#{h(event.name)}">)
+      html << display_event_time(event, day)
+      html << %(#{h(event.name)}</a>)
+      html
     end
   end
 end
