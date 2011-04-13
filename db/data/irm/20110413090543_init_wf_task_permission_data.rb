60afb0734656d8d8595d402bb4de37a012b90ac6
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-04-13 18:06:14 +0800

    事件　菜单和权限初始化数据

--- db/data/irm/20110413090543_init_wf_task_permission_data.rb
+++ db/data/irm/20110413090543_init_wf_task_permission_data.rb
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 class InitWfTaskPermissionData < ActiveRecord::Migration
   def self.up
-    irm_edit_wf_tasks= Irm::Function.new(:group_code=>'IRM_WF_TASKS',:function_code=>'EDIT_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
+    irm_edit_wf_tasks= Irm::Function.new(:group_code=>'IRM_SYSTEM_HOME_PAGE',:function_code=>'EDIT_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
     irm_edit_wf_tasks.functions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'编辑任务',:description=>'编辑任务')
     irm_edit_wf_tasks.functions_tls.build(:language=>'en',:source_lang=>'en',:name=>'Edit Task',:description=>'Edit Task')
     irm_edit_wf_tasks.save
-    irm_create_wf_tasks= Irm::Function.new(:group_code=>'IRM_WF_TASKS',:function_code=>'CREATE_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
+    irm_create_wf_tasks= Irm::Function.new(:group_code=>'IRM_SYSTEM_HOME_PAGE',:function_code=>'CREATE_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
     irm_create_wf_tasks.functions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'新建任务',:description=>'新建任务')
     irm_create_wf_tasks.functions_tls.build(:language=>'en',:source_lang=>'en',:name=>'New Task',:description=>'New Task')
     irm_create_wf_tasks.save
-    irm_view_wf_tasks= Irm::Function.new(:group_code=>'IRM_WF_TASKS',:function_code=>'VIEW_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
+    irm_view_wf_tasks= Irm::Function.new(:group_code=>'IRM_SYSTEM_HOME_PAGE',:function_code=>'VIEW_WF_TASKS',:default_flag=>'N',:public_flag=>'N',:not_auto_mult=>true)
     irm_view_wf_tasks.functions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'任务',:description=>'任务')
     irm_view_wf_tasks.functions_tls.build(:language=>'en',:source_lang=>'en',:name=>'View Task',:description=>'View Task')
     irm_view_wf_tasks.save
-
-    irm_irm_wf_task= Irm::FunctionGroup.new(:group_code=>'IRM_WF_TASK',:not_auto_mult=>true)
-    irm_irm_wf_task.function_groups_tls.build(:language=>'zh',:source_lang=>'en',:name=>'任务',:description=>'任务')
-    irm_irm_wf_task.function_groups_tls.build(:language=>'en',:source_lang=>'en',:name=>'Workflow Task',:description=>'Workflow Task')
-    irm_irm_wf_task.save
 
     irm_menu_entiry_113= Irm::MenuEntry.new(:menu_code=>'IRM_HOME_MENU',:sub_menu_code=>nil,:page_controller=>'irm/wf_tasks',:display_sequence=>80,:display_flag=>'N',:not_auto_mult=>true)
     irm_menu_entiry_113.menu_entries_tls.build(:language=>'zh',:source_lang=>'en',:name=>'任务',:description=>'任务')
