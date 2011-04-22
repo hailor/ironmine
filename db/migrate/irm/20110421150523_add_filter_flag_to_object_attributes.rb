9d2e2fb5ff6728cf5c791744455d46fd58e67c14
hailor <hailor1985@gmail.com>
2011-04-22 10:22:09 +0800

    添加过滤标志字段

--- db/migrate/irm/20110421150523_add_filter_flag_to_object_attributes.rb
+++ db/migrate/irm/20110421150523_add_filter_flag_to_object_attributes.rb
@@ -1,14 +1,14 @@
 class AddFilterFlagToObjectAttributes < ActiveRecord::Migration
   def self.up
     add_column :irm_object_attributes, :filter_flag, :string,:default=>"N",:limit=>1, :after => :approval_page_field_flag
-    execute('CREATE OR REPLACE VIEW irm_object_attributes_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.value_title,tl.desc_title,tl.addition_title,tl.language,tl.source_lang
+    execute('CREATE OR REPLACE VIEW irm_object_attributes_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
                                              FROM irm_object_attributes  t,irm_object_attributes_tl tl
                                              WHERE t.id = tl.object_attribute_id')
   end
 
   def self.down
     remove_column :irm_object_attributes, :filter_flag
-    execute('CREATE OR REPLACE VIEW irm_object_attributes_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.value_title,tl.desc_title,tl.addition_title,tl.language,tl.source_lang
+    execute('CREATE OR REPLACE VIEW irm_object_attributes_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
                                              FROM irm_object_attributes  t,irm_object_attributes_tl tl
                                              WHERE t.id = tl.object_attribute_id')
   end
