83e07b9a49008898264c925ec2885347bc7d3c73
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-04-28 02:03:41 +0800

    报表INDEX页面改造

--- app/controllers/irm/report_lists_controller.rb
+++ app/controllers/irm/report_lists_controller.rb
@@ -1,4 +1,21 @@
 class Irm::ReportListsController < ApplicationController
   def index
+    @report_category = params[:report_category] if params[:report_category]
+  end
+
+  def get_data
+    category_code = ""
+    category_code = params[:report_category] if params[:report_category] && !params[:report_category].blank?
+
+    report_group_codes = Irm::Person.current.hidden_roles.collect{|r| r.report_group_code}
+    report_group_codes << Irm::Role.current.report_group_code if Irm::Role.current
+    reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
+        query_by_report_purpose("REPORT").query_by_category_code(category_code)
+
+    reports_scope = reports_scope.match_value("#{Irm::ReportsTl.table_name}.name",params[:name])
+    reports,count = paginate(reports_scope)
+    respond_to do |format|
+      format.json {render :json=>to_jsonp(reports.to_grid_json([:name,:description, :page_controller, :page_action],count))}
+    end
   end
 end
