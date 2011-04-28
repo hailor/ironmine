5fd15755cbfac0382aa28f3f0ea901adce0fd770
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-04-28 10:27:43 +0800

    报表列表页面样式调整

--- app/controllers/irm/report_lists_controller.rb
+++ app/controllers/irm/report_lists_controller.rb
@@ -9,13 +9,22 @@ class Irm::ReportListsController < ApplicationController
 
     report_group_codes = Irm::Person.current.hidden_roles.collect{|r| r.report_group_code}
     report_group_codes << Irm::Role.current.report_group_code if Irm::Role.current
-    reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
-        query_by_report_purpose("REPORT").query_by_category_code(category_code)
+    if category_code.blank?
+      reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
+        query_by_report_purpose("REPORT").with_blank_url
+    else
+      reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
+          query_by_report_purpose("REPORT").query_by_category_code(category_code).with_blank_url
+    end
+
 
     reports_scope = reports_scope.match_value("#{Irm::ReportsTl.table_name}.name",params[:name])
-    reports,count = paginate(reports_scope)
+    @reports,count = paginate(reports_scope)
+    @reports.each do |r|
+      r.url = url_for(:controller => r[:page_controller], :action => r[:page_action])
+    end
     respond_to do |format|
-      format.json {render :json=>to_jsonp(reports.to_grid_json([:name,:description, :page_controller, :page_action],count))}
+      format.json {render :json=>to_jsonp(@reports.to_grid_json([:name,:description, :page_controller, :page_action, :url],count))}
     end
   end
 end
