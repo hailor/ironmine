7c5f6c031b3e70e717c05fe05fc2cc94c01ac91f
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-05-06 13:45:35 +0800

    知识库权限整理

--- app/controllers/irm/report_lists_controller.rb
+++ app/controllers/irm/report_lists_controller.rb
@@ -9,14 +9,14 @@ class Irm::ReportListsController < ApplicationController
 
     report_group_codes = Irm::Person.current.hidden_roles.collect{|r| r.report_group_code}
     report_group_codes << Irm::Role.current.report_group_code if Irm::Role.current
+
     if category_code.blank?
       reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
-        query_by_report_purpose("REPORT").with_blank_url
+        query_by_report_purpose("REPORT").with_blank_url.with_category(I18n.locale)
     else
       reports_scope = Irm::Report.multilingual.query_by_group_codes(report_group_codes).
-          query_by_report_purpose("REPORT").query_by_category_code(category_code).with_blank_url
+          query_by_report_purpose("REPORT").query_by_category_code(category_code).with_blank_url.with_category(I18n.locale)
     end
-
 
     reports_scope = reports_scope.match_value("#{Irm::ReportsTl.table_name}.name",params[:name])
     @reports,count = paginate(reports_scope)
@@ -24,7 +24,7 @@ class Irm::ReportListsController < ApplicationController
       r.url = url_for(:controller => r[:page_controller], :action => r[:page_action])
     end
     respond_to do |format|
-      format.json {render :json=>to_jsonp(@reports.to_grid_json([:name,:description, :page_controller, :page_action, :url],count))}
+      format.json {render :json=>to_jsonp(@reports.to_grid_json([:name,:description, :page_controller, :page_action, :url, :category_name],count))}
     end
   end
 end
