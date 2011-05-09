66bdf3f52fc419a6e4d58d23cdd233968eb4f088
Stanley Liu <stanleyliu@localhost.(none)>
2011-05-09 09:40:36 +0800

    add skm file permissions temp...

--- lib/skm/init_permission.rb
+++ lib/skm/init_permission.rb
@@ -19,17 +19,17 @@ Irm::AccessControl.map do |map|
   map.function :edit_entry_template_elements,{"skm/entry_template_elements"=>["edit", "update"]}
 
   #===================skm/settings ====================================
-  map.function :view_skm_settings, {"skm/settings" => ["index"],
-                                    "skm/file_managements" => ["index", "new", "create", "batch_create", "edit", "update",
-                                                               "get_data", "destroy", "get_version_files", "remove_version_file"]}
+  map.function :view_skm_settings, {"skm/settings" => ["index"]}
   map.function :edit_skm_settings, {"skm/settings" => ["edit", "update"]}
 
 
   #===================skm/entries ==========================================
   map.function :view_skm_entries, {"skm/entry_headers" => ["index", "show", "index_search", "get_history_entries_data", "get_data",
-                                                           "my_favorites_data", "my_favorites", "add_favorites", "data_grid"],
+                                                           "my_favorites_data", "my_favorites", "add_favorites", "data_grid", "my_drafts", "my_drafts_data"],
                                    "icm/incident_journals" => ["get_entry_header_data", "apply_entry_header"],
-                                   "skm/entry_templates" => ["get_owned_elements_data"]}
+                                   "skm/entry_templates" => ["get_owned_elements_data"],
+                                   "skm/file_managements" => ["index", "new", "create", "batch_create", "edit", "update",
+                                                              "get_data", "destroy", "get_version_files", "remove_version_file"]}
   map.function :create_skm_entries, {"skm/entry_headers" => ["new", "create", "new_step_1", "new_step_2", "new_step_3", "new_step_4", "new_from_icm_request"]}
   map.function :edit_skm_entries, {"skm/entry_headers" => ["edit", "update"]}
 
