2a0aa411c1616f8d0ed953d19ca31c988d6755a7
hailor <hailor1985@gmail.com>
2011-05-12 22:28:50 +0800

    修改级联下拉框方法

--- public/javascripts/irm/cascadeselect.js
+++ public/javascripts/irm/cascadeselect.js
@@ -1,4 +1,5 @@
 YUI.add('cascadeselect', function(Y) {
+    var OPTIONS_NODE_TEMPLATE = "<option></option>";
     var OPTIONS_TEMPLATE = "<option value={value}>{label}</option>";
     function CascadeSelect(config){
         CascadeSelect.superclass.constructor.apply(this, arguments);
@@ -21,8 +22,50 @@ YUI.add('cascadeselect', function(Y) {
         initializer: function(config) {
           var source = Y.one(this.get("source"));
           source.on(this.get("event"), Y.bind(this._onSourceEvent,this));
-
+          this.initTarget();
         },
+        initTarget: function(){
+           var targets = this.get("target");
+           for(var i=0;i<targets.length;i++){
+               var target = Y.one(targets[i]);
+               if(target.all("option").length>1)
+                    continue;
+               var href = target.getAttribute("href");
+               var depends = target.getAttribute("depend");
+               if(depends!="")
+                   depends=depends.split(",");
+               else
+                   depends = [];
+               var url_options = {};
+               var value_length = 0;
+
+               for(var j=0;j<depends.length;j++){
+                 if(Y.one("#"+depends[j]).get("value")&&Y.one("#"+depends[j]).get("value")!=""){
+                   url_options[depends[j]] = Y.one("#"+depends[j]).get("value");
+                   value_length++;
+                 }
+               }
+
+               if(value_length==depends.length||value_length>0){
+                 href = Y.Lang.substitute(unescape(href),url_options);
+                 var cfg = {
+                     on: {complete: this._prepareOptions},
+                     arguments: {complete:target}
+                 };
+                 Y.io(href, cfg);
+
+               }else
+               {
+                 var options = "";
+                 if(target.getAttribute("blank")!="")
+                   options =  options+Y.Lang.substitute(OPTIONS_TEMPLATE,{label:target.getAttribute("blank"),value:""}) ;
+                 target.setContent(options);
+                 target.set("value","");
+                 target.simulate("change");
+               }
+           }
+        }
+        ,
         _onSourceEvent : function(e){
            var targets = this.get("target");
            for(var i=0;i<targets.length;i++){
@@ -65,15 +108,29 @@ YUI.add('cascadeselect', function(Y) {
         },
         _prepareOptions: function(transactionId, response, arg){
           var results = Y.JSON.parse(response.responseText);
-          var options = "";
+          var target = arg["complete"];
+          target.setContent("");
+
+          if(target.getAttribute("blank")!=""){
+             var option  =  Y.Node.create(Y.Lang.substitute(OPTIONS_TEMPLATE,{label:target.getAttribute("blank"),value:""})) ;
+             target.appendChild(option)
+          }
+
           var items = results["items"];
           for(var o in items){
-             options += Y.Lang.substitute(OPTIONS_TEMPLATE,items[o]);
+             var option = Y.Node.create(OPTIONS_NODE_TEMPLATE);
+             for(var v in items[o])
+             {
+                 if(v=="label")
+                    option.setContent(items[o][v]);
+                 else
+                    option.setAttribute(v,items[o][v]);
+             }
+             target.appendChild(option)
           }
-          var target = arg["complete"];
-          if(target.getAttribute("blank")!="")
-             options =  Y.Lang.substitute(OPTIONS_TEMPLATE,{label:target.getAttribute("blank"),value:""})+options ;
-          target.setContent(options);
+
+
+          //target.setContent(options);
           target.set("value","");
           target.simulate('change');
         }
