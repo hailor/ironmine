78028c77e16c4036941879b4185230a623c1ddc1
hailor <hailor1985@gmail.com>
2011-05-25 13:42:49 +0800

    添加邮件模板helper方法

--- app/helpers/irm/mail_templates_helper.rb
+++ app/helpers/irm/mail_templates_helper.rb
@@ -2,4 +2,7 @@ module Irm::MailTemplatesHelper
   def available_mail_template
     Irm::MailTemplate.multilingual.enabled.collect{|i| [i[:name],i.template_code]}
   end
+  def available_mail_template_ids
+    Irm::MailTemplate.multilingual.enabled.collect{|i| [i[:name],i.id]}
+  end
 end
